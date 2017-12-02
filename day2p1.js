d29de522add3265f067cbea946d86d0b66bc0a36
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2017-12-02 00:12:53 -0500

    Day 2 Part 1

---
+++ day2p1.js
@@ -0,0 +1,32 @@
+var fs = require('fs')
+
+var spreadsheet = fs.readFileSync('day2input.txt', {encoding: 'utf8'}).split('\n')
+
+var sum = 0
+
+for (var i = 0; i < spreadsheet.length; i++) {
+  if (spreadsheet[i] === '') {
+    continue
+  }
+
+  var row = spreadsheet[i].split(' ')
+
+  var min = Number.MAX_VALUE
+  var max = Number.MIN_VALUE
+
+  for (var j = 0; j < row.length; j++ ) {
+    var num = parseInt(row[j], 10)
+
+    if (num < min) {
+      min = num
+    }
+
+    if (num > max) {
+      max = num
+    }
+  }
+
+  sum += max - min
+}
+
+console.log(sum)
