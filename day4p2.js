4e6b90e104a1cb72c05f532d2cdf10c03d61d462
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2017-12-04 00:18:01 -0500

    Day 4 Part 2

---
+++ day4p2.js
@@ -0,0 +1,62 @@
+var fs = require('fs')
+
+var passphrases = fs.readFileSync('day4input.txt', {encoding: 'utf8'}).split('\n')
+
+var parts
+var memo
+var validCount = 0
+var valid
+
+function distribution(word) {
+  var res = {}
+  for (var i = 0; i < word.length; i++) {
+    if (!res[word.charAt(i)]) {
+      res[word.charAt(i)] = 0
+    }
+    res[word.charAt(i)]++
+  }
+  return res
+}
+
+for (var i = 0; i < passphrases.length; i++) {
+  if (passphrases[i] === '') {
+    continue
+  }
+
+  parts = passphrases[i].split(' ')
+
+  valid = true
+
+allLoops:
+  for (var j = 0; j < parts.length; j++) {
+    for (var k = j + 1; k < parts.length; k++) {
+      var a = distribution(parts[j])
+      var b = distribution(parts[k])
+
+      var anagram = true
+      for (var key in a) {
+        if (a[key] !== b[key]) {
+          anagram = false
+          break
+        }
+      }
+      for (var key in b) {
+        if (a[key] !== b[key]) {
+          anagram = false
+          break
+        }
+      }
+
+      if (anagram) {
+        valid = false
+        break allLoops
+      }
+    }
+  }
+
+  if (valid) {
+    validCount++
+  }
+}
+
+console.log(validCount)
