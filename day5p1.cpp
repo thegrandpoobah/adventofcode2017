bb69b8ec8f450c21bcbc43255e0dcb76c6896dfa
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2017-12-05 00:27:22 -0500

    Day 5 Part 1 (in C++!)

---
+++ day5p1.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+int main() {
+  FILE *fp = fopen("day5input.txt", "rt");
+  if (fp == NULL) {
+    exit(-1);
+  }
+
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  std::vector<int> list;
+
+  while ((read = getline(&line, &len, fp)) != -1) {
+    list.push_back(atoi(line));
+  }
+
+  int position = 0;
+  int steps = 0;
+
+  while (true) {
+    steps++;
+
+    int delta = list[position];
+    list[position]++;
+    position += delta;
+
+    if (position < 0 || position > list.size() - 1) {
+      break;
+    }
+  }
+
+  printf("%i\n", steps);
+
+  fclose(fp);
+  if (line) {
+    free(line);
+  }
+
+  exit(0);
+}
